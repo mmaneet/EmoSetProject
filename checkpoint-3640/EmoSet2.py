a4abe7fbabc2b2e4e1807e00b0c6f79f79c4803d
manee <mmaneet2007@gmail.com>
2025-06-16 00:03:36 -0400

    Add checkpoints via LFS

---
+++ checkpoint-3640/EmoSet2.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3ee15f45ee9b2b13c8c2a6ed604a48d70487e8de84098ae3daa63c0b310d7dea
+size 4550
---
+++ checkpoint-3640/ViT Validator.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2b27d9eb430b3cc59c1cdca4c6a3bc7c0e639f6607e5d512fa3a5cd0a8551914
+size 2728
